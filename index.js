c1bde12b1c6a7e0d73296bd39bf5f16af23bea78
Noelle Foster <uufarmgirl@gmail.com>
2018-07-21 15:49:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function nowServing(katzDeliLine){
     return `Currently serving ${katzDeliLine.pop()}.`
   }
 }
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length===0){
+    return "The line is currently empty."
+  }
+  else{
+    newArr = new Array()
+    for (var i = 0; i<katzDeliLine.length; i++){
+      str=`${i+1}. ${katzDeliLine[i]}`
+    }
+
+
+}
