cf146bac8a2ca065151dc3f8f9e20e2fa7c418a0
Noelle Foster <uufarmgirl@gmail.com>
2018-07-21 16:06:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,13 @@ function currentLine(katzDeliLine){
     var newArr= new Array()
     var str = ""
     for (var i = 0; i<katzDeliLine.length; i++){
+      if(i===0){
+      str=`${i+1}. ${katzDeliLine[i]}`
+    }
+    else{
       str=` ${i+1}. ${katzDeliLine[i]}`
-      newArr[i]=str
+    }
+    newArr[i]=str
     }
     return "The line is currently: " +newArr
   }
