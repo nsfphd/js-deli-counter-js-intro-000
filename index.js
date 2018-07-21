808116499ee14a0cade8a92ba0e66790c2562382
Noelle Foster <uufarmgirl@gmail.com>
2018-07-21 15:41:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var katzDeliLine = new Array()
 
 function takeANumber(katzDeliLine, name){
-  str= `Welcome, ${name}.  You are number ${katzDeliLine.length+1} in line.`
   katzDeliLine.push(name)
-  return str
+  return `Welcome, ${name}.  You are number ${katzDeliLine.length} in line.`
 }
